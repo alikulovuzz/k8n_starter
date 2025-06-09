c4fe42c7d595cd9989c25246cf3da556a6371fc8
azamat aliqulov <azamat.aliqulov@list.ru>
2025-06-09 14:34:27 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+const app = express();
+const port = 3000;
+
+// Asosiy endpoint
+app.get('/', (req, res) => {
+  res.send('✅ Hello from Node.js HPA test!');
+});
+
+// Yuklaydigan endpoint
+app.get('/load', (req, res) => {
+  const end = Date.now() + 5000; // 5 soniya CPU yuk
+  while (Date.now() < end) {
+    // CPUni band qilish (CPU intensive loop)
+    Math.sqrt(Math.random() * Math.random());
+  }
+  res.send('🌀 CPU load finished (5s)');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
