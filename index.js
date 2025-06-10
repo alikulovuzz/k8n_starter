3eec4b79c9a3ce2c8837615b061f7744b9b626b1
azamat aliqulov <azamat.aliqulov@list.ru>
2025-06-10 10:48:35 +0500

    new commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 3000;
 
 // Asosiy endpoint
 app.get('/', (req, res) => {
-  res.send('✅ Hello from Node.js HPA test! 14:509');
+  res.send('✅ Hello from Node.js HPA test! yangilanish');
 });
 
 // Yuklaydigan endpoint
